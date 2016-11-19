751a0f210e8cfa4fc17b9a7e41044d5420b15907
HopeIsEverything <hopeiseverythingaswell@gmail.com>
2016-11-18 22:31:04 -0600

    I have no idea how to execute code and I'm just playing around at this point.

--- resources/codesign/compile.sh
+++ resources/codesign/compile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sandbox="-alias,_kCFBooleanTrue,_SANDBOX_CHECK_NO_REPORT,"
+sandbox+="-alias,_APP_SANDBOX_READ,_APP_SANDBOX_READ,"
 sandbox+="-alias,_sync,_sandbox_check,"
 sandbox+="-alias,_sync,_sandbox_extension_consume,"
 sandbox+="-alias,_sync,_sandbox_extension_issue_file,"
@@ -21,7 +22,7 @@ amfi+="-alias,_kCFUserNotificationTokenKey,_kMISValidationOptionValidateSignatur
 
 isysroot_location="/Applications/Xcode-5.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS6.1.sdk"
 
-gcc -dynamiclib base.m -o sandbox.dylib -Wl,$sandbox -arch armv7 -isysroot $isysroot_location -framework CoreFoundation -framework Foundation -lm -install_name /usr/lib/system/libsystem_sandbox.dylib -current_version 1.0 -compatibility_version 1.0
+gcc -dynamiclib base.m -o sandbox.dylib -Wl,$sandbox -arch armv7 -isysroot $isysroot_location -framework CoreFoundation -framework Foundation -lm -lsandbox -install_name /usr/lib/system/libsystem_sandbox.dylib -current_version 1.0 -compatibility_version 1.0
 gcc -dynamiclib base.m -o amfi.dylib -Wl,$amfi -arch armv7 -isysroot $isysroot_location -framework CoreFoundation -framework Foundation -lm -install_name /usr/lib/libmis.dylib -current_version 1.0 -compatibility_version 1.0
 
 echo "Compiled successfully!"
